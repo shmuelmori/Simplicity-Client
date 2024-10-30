9bd4446e1a29031c74f4384988be1f8a3830a4b9
shmuelmori <shmuelmuri@gmail.com>
2024-10-30 09:53:40 +0200

    fix loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 
 export default function Loading() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center w-full h-full">
             <div className="relative">
                 <div className="w-24 h-24 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
                 <span className="absolute inset-0 flex items-center justify-center text-blue-500 font-semibold p-2">
