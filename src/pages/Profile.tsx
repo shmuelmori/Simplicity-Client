f82ca8d93bf23bf330f47a381d7ad9b44389c450
shmuelmori <shmuelmuri@gmail.com>
2024-10-31 23:15:55 +0200

    levy rozman

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -90,7 +90,7 @@ const Profile = () => {
                                 <img
                                     src={user.icon}
                                     alt="Profile"
-                                    className="w-24 h-24 rounded-full object-cover border-4 border-primary"
+                                    className="w-40 h-40 sm:w-24 sm:h-24 rounded-full object-cover border-4 border-primary mt-10 sm:mt-2"
                                 />
                             ) : (
                                 <div className="w-24 h-24 rounded-full bg-primary flex items-center justify-center text-white text-2xl">
